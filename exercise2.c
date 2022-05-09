2c9c25a972a6a5fc2f9b0610f0613f0604e11dd6
liyanazamri1812 <93965384+liyanazamri1812@users.noreply.github.com>
2022-05-09 12:25:03 +0800

    Update exercise2.c

--- exercise2.c
+++ exercise2.c
@@ -5,11 +5,22 @@
 #include<stdio.h>
 
 //global variable section 
-
+int Fibonacci (int N);
+int i;
+int N = 10;
 
 //main function section 
 int main(){
     //write here your program
+    
+        printf("The Series are: ");
+    for (i = 1; i <= N; i++)
+    {
+        printf("%d ", Fibonacci (i));
+    }
+    
+    return 0;
+}
 
 
     return 0;
@@ -17,3 +28,22 @@ int main(){
 
 
 //user-defined section 
+int Fibonacci (int N)
+{
+    if (N<= 0)
+    {
+        return 0;
+    }
+
+    else if( N==1 )
+    {
+        return 1;
+    }
+   
+    else
+    {
+        return Fibonacci (N-1)+Fibonacci (N-2);
+    }
+        
+}
+    
